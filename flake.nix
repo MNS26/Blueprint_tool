1fc87e4d3ff9f15139c39bce07e37712f0948ece
Michael Bishop <cleverca22@gmail.com>
2025-02-17 22:26:21 +0000

    fix 32bit windows

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
   {
     packages = {
       aarch64-multiplatform.default = nixpkgs.legacyPackages.x86_64-linux.pkgsCross.aarch64-multiplatform.callPackage ./. {};
-      mingw32.default = nixpkgs.legacyPackages.x86_64-linux.pkgsCross.mingw32.callPackage ./. {};
+      mingw32.default = (nixpkgs.legacyPackages.x86_64-linux.extend overlay).pkgsCross.mingw32.callPackage ./. {};
       mingwW64.default = (nixpkgs.legacyPackages.x86_64-linux.extend overlay).pkgsCross.mingwW64.callPackage ./. {};
       x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.callPackage ./. {};
     };
